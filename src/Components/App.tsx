884aa12198ed13439dd2ad199da1128ceb2b1889
Logan Matheny <loganpaulmatheny@gmail.com>
2024-01-19 10:24:37 -0500

    Create NewNote Component

---
+++ src/Components/App.tsx
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
+import NewNote from "./NewNote"
+
+function App() {
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>}/>
+        <Route path="/new" element={<NewNote />}/>
+        <Route path=":id">
+          <Route index element={<h2>Show</h2>} /> 
+          <Route path="edit" element={<h2>Edit</h2>} /> 
+        </Route>
+        <Route path="/*" element={<Navigate to="/" /> } />
+      </Routes>
+    </Container>
+  )
+}
+
+export default App
